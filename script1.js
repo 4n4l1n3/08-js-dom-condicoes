134f04f5bf08b7d72401000f7d16be0ee1dfd757
Analine <aas27@aluno.ifnmg.edu.br>
2023-06-29 20:51:22 -0300

    Resolvendo Verificador de Idade

---
+++ script1.js
@@ -0,0 +1,57 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'criancaM.png')
+                document.body.style.background = rgb(81, 174, 236);
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovemM.png')
+                document.body.style.background = rgb(81, 174, 236);
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adultoM.png')
+                document.body.style.background = rgb(81, 174, 236);
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosoM.png')
+                document.body.style.background = rgb(81, 174, 236);
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'criancaF.png')
+                document.body.style.background = rgb(224, 128, 208);
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovemF.png')
+                document.body.style.background = rgb(224, 128, 208);
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adultoF.png')
+                document.body.style.background = rgb(224, 128, 208);
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosoF.png')
+                document.body.style.background = rgb(224, 128, 208);
+            }
+        }
+        res.computedStyleMap.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
